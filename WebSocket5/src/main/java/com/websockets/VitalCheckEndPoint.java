13b346fab44a4b3a5239bbc8f9e8e695049e762a
sarayu06 <67803457+sarayu06@users.noreply.github.com>
2021-06-27 21:02:04 +0530

    Add files via upload

---
+++ WebSocket5/src/main/java/com/websockets/VitalCheckEndPoint.java
@@ -0,0 +1,96 @@
+package com.websockets;
+
+import javax.websocket.server.ServerEndpoint;
+
+import jdk.jshell.spi.ExecutionControl.ExecutionControlException;
+
+import java.util.Collections;
+import java.io.StringWriter;
+import java.util.*;
+import javax.websocket.*;
+import javax.json.*;
+
+@ServerEndpoint(value="/VitalCheckEndPoint",configurator=VitalCheckConfigurator.class)
+public class VitalCheckEndPoint {
+	static Set<Session> subscribers=Collections.synchronizedSet(new HashSet<Session>());
+	@OnOpen
+	public void handleOpen(EndpointConfig endpointconfig,Session userSession) {
+		userSession.getUserProperties().put("username", endpointconfig.getUserProperties().get("username"));
+		subscribers.add(userSession);
+	}
+	
+	@OnMessage
+	public void handleMessage(String Message,Session userSession) {
+		String username=(String)userSession.getUserProperties().get("username");
+		
+		if(username!=null && !username.equals("doctor")) {
+			subscribers.stream().forEach(x->{
+				
+				try {
+					
+					if(x.getUserProperties().get("username").equals("doctor")) {
+						x.getBasicRemote().sendText(buildJSON(username,Message));
+						
+					}
+				}
+				catch(Exception e) {
+					e.getStackTrace();
+				}
+				
+				
+			});
+		}
+		else if(username!=null && username.equals("doctor")) {
+			String[] messages=Message.split(",");
+			String patient=messages[0];
+			String subject=messages[1];
+			
+			subscribers.stream().forEach(x->{
+				try {
+					if(subject.equals("ambulance")) {
+						if(x.getUserProperties().get("username").equals(patient)) {
+							x.getBasicRemote().sendText(buildJSON("doctor","has summoned an ambulance"));
+						
+						}
+						else if(x.getUserProperties().get("username").equals("ambulance")) {
+							x.getBasicRemote().sendText(buildJSON(patient,"Requires an ambulance"));
+						
+						}
+					}
+					else if(subject.equals("medication")) {
+						if(x.getUserProperties().get("username").equals(patient)) {
+							x.getBasicRemote().sendText(buildJSON("doctor",messages[2]+","+messages[3]));
+						}
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			});
+		}
+		
+		}
+	@OnClose
+	public void handleClose(Session userSession) {
+		subscribers.remove(userSession);
+		
+	}
+	@OnError
+	public void handleError(Throwable t) {
+		
+	}
+	private String buildJSON(String username,String message) {
+		
+		JsonObject jsonObject=Json.createObjectBuilder().add("message",username+","+message).build();
+		StringWriter stringWriter=new StringWriter();
+		try(JsonWriter jsonWriter=Json.createWriter(stringWriter))
+		{
+			jsonWriter.write(jsonObject);
+		}
+		
+		return stringWriter.toString();
+	}
+	
+	
+	
+}
